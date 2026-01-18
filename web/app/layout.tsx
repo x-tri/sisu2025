d3d3e43c2a2f7862a26bd777b7500622abed3143
Vercel <vercel[bot]@users.noreply.github.com>
2026-01-18 14:31:41 +0000

    Install Vercel Web Analytics for Next.js
    This is an automated pull request generated by Vercel to help install [Web Analytics](https://vercel.com/docs/analytics) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/next'
 
 import { ScoreProvider } from '../context/ScoreContext'
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body>
         <ScoreProvider>{children}</ScoreProvider>
+        <Analytics />
       </body>
     </html>
   )
